39d034fdb854999e3e7ae961c8e3de9f1d33c153
Tej Chajed <tchajed@mit.edu>
2021-02-28 21:44:52 -0600

    Make installation script more robust

--- install-dafny.sh
+++ install-dafny.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+set -e
+
 VERSION="$1"
+
+if [ -z "$VERSION" ]; then
+   echo "No version provided" >&2
+   exit 1
+fi
 
 cd
 wget -O dafny.zip "https://github.com/dafny-lang/dafny/releases/download/v${VERSION}/dafny-${VERSION}-x64-ubuntu-16.04.zip"
