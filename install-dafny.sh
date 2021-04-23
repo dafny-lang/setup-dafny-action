67d9b7d02ca51aace4a211246b6ca56fc660b32a
Tej Chajed <tchajed@mit.edu>
2021-04-23 09:56:04 -0400

    Remove dafny.zip after extracting

--- install-dafny.sh
+++ install-dafny.sh
@@ -12,4 +12,5 @@ fi
 cd
 wget -O dafny.zip "https://github.com/dafny-lang/dafny/releases/download/v${VERSION}/dafny-${VERSION}-x64-ubuntu-16.04.zip"
 unzip dafny.zip
+rm dafny.zip
 echo "$HOME/dafny" >> "$GITHUB_PATH"
