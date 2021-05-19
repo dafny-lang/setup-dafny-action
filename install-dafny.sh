d953e9df3d79a4cb8d724ae5a90aa7efaa786c8a
Tej Chajed <tchajed@mit.edu>
2021-05-19 09:36:58 -0400

    Auto-format install-dafny.sh

--- install-dafny.sh
+++ install-dafny.sh
@@ -5,12 +5,12 @@ set -e
 VERSION="$1"
 
 if [ -z "$VERSION" ]; then
-   echo "No version provided" >&2
-   exit 1
+  echo "No version provided" >&2
+  exit 1
 fi
 
 cd
 wget -O dafny.zip "https://github.com/dafny-lang/dafny/releases/download/v${VERSION}/dafny-${VERSION}-x64-ubuntu-16.04.zip"
 unzip dafny.zip
 rm dafny.zip
-echo "$HOME/dafny" >> "$GITHUB_PATH"
+echo "$HOME/dafny" >>"$GITHUB_PATH"
