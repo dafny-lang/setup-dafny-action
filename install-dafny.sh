6fab553d02f10e4c8d93c7e22a382a12f438dc99
Tej Chajed <tchajed@mit.edu>
2021-05-19 10:56:48 -0400

    Fix support for Dafny 2.3.0 and 2.2.0

--- install-dafny.sh
+++ install-dafny.sh
@@ -18,7 +18,14 @@ if [ "$uname_s" = "Darwin" ]; then
   os_name="osx-10.14.2"
 fi
 
-wget -O dafny.zip "https://github.com/dafny-lang/dafny/releases/download/v${VERSION}/dafny-${VERSION}-x64-${os_name}.zip"
+filename=dafny-${VERSION}-x64-${os_name}.zip
+if [ "$VERSION" = "2.3.0" ]; then
+  filename="dafny-2.3.0.10506-x64-${os_name}.zip"
+elif [ "$VERSION" = "2.2.0" ]; then
+  filename="dafny-2.2.0.10923-x64-${os_name}.zip"
+fi
+
+wget -O dafny.zip "https://github.com/dafny-lang/dafny/releases/download/v${VERSION}/$filename"
 unzip dafny.zip
 rm dafny.zip
 
