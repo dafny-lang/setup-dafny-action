23dc9644ec16c0ba8c743f303885205a4aae0b3d
Tej Chajed <tchajed@mit.edu>
2021-05-19 09:41:05 -0400

    Add support for macOS

--- install-dafny.sh
+++ install-dafny.sh
@@ -10,7 +10,19 @@ if [ -z "$VERSION" ]; then
 fi
 
 cd
-wget -O dafny.zip "https://github.com/dafny-lang/dafny/releases/download/v${VERSION}/dafny-${VERSION}-x64-ubuntu-16.04.zip"
+
+uname_s=$(uname -s)
+
+os_name="ubuntu-16.04"
+if [ "$uname_s" = "Darwin" ]; then
+  os_name="osx-10.14.2"
+fi
+
+wget -O dafny.zip "https://github.com/dafny-lang/dafny/releases/download/v${VERSION}/dafny-${VERSION}-x64-${os_name}.zip"
 unzip dafny.zip
 rm dafny.zip
+if [ "$uname_s" = "Darwin" ]; then
+  ~/dafny/allow_on_mac.sh
+fi
+
 echo "$HOME/dafny" >>"$GITHUB_PATH"
