ec86ef2cc4a6e2e77923aea55678d1c611b1d259
Tej Chajed <tchajed@mit.edu>
2021-05-19 09:44:25 -0400

    Stop running allow_on_mac.sh
    Seems to not do anything on GitHub actions?

--- install-dafny.sh
+++ install-dafny.sh
@@ -5,8 +5,8 @@ set -e
 VERSION="$1"
 
 if [ -z "$VERSION" ]; then
-  echo "No version provided" >&2
-  exit 1
+	echo "No version provided" >&2
+	exit 1
 fi
 
 cd
@@ -15,14 +15,11 @@ uname_s=$(uname -s)
 
 os_name="ubuntu-16.04"
 if [ "$uname_s" = "Darwin" ]; then
-  os_name="osx-10.14.2"
+	os_name="osx-10.14.2"
 fi
 
 wget -O dafny.zip "https://github.com/dafny-lang/dafny/releases/download/v${VERSION}/dafny-${VERSION}-x64-${os_name}.zip"
 unzip dafny.zip
 rm dafny.zip
-if [ "$uname_s" = "Darwin" ]; then
-  ~/dafny/allow_on_mac.sh
-fi
 
 echo "$HOME/dafny" >>"$GITHUB_PATH"
