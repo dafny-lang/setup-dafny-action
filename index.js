4ca29d719f9f4e452f9a68abe037f43d575d792e
Jose Corella <joseeduardocorella@gmail.com>
2024-07-12 16:36:46 -0700

    fix: add support for downloading arm64 macos image

--- index.js
+++ index.js
@@ -62,9 +62,10 @@ async function dafnyURLAndFullVersion(version, distribution) {
     fullVersion = version;
   }
   const root = "https://github.com/dafny-lang/dafny/releases/download";
+  // using the same approach as the dafny-lang/ide-vscode
   const url = `${root}/${versionPath}/dafny-${
     version == "2.3.0" ? "2.3.0.10506" : version
-  }-x64-${distribution}.zip`;
+  }-${os.arch()}-${distribution}.zip`;
   return { url, fullVersion };
 }
 
